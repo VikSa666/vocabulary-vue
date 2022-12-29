fe6a2349387382ebaf7151d5761e19f8a533a083
victor-at-wiris <victor.santiago@wiris.com>
2022-12-29 12:13:34 +0100

    feat: implement first naive version of the submitting word

--- vocabulary-project/src/App.vue
+++ vocabulary-project/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <h1>Hello World</h1>
   <div>
-    <div v-for="(value, key) in myJson">
-      <div v-for="(english, russian) in value">KEY: {{ key }} VALUE: english {{ english }} russian {{ russian }}</div>
+    <div v-for="(dictionary, cathegory) in myJson">
+      <p>{{ cathegory }}</p>
+      <div v-for="(russian, english) in dictionary">
+        <form @submit.prevent="submitWord">
+          <input type="text" @input="readWord($event, russian)">
+        </form>
+
+        <div v-if="submitted">Your answer was {{ answer }} and the actual answer was {{ english }}. Therefore, you are
+          {{ correct }}.
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -12,7 +22,19 @@ import json from './assets/vocab.json'
 export default {
   data() {
     return {
-      myJson: json
+      myJson: json,
+      correct: false,
+      answer: '',
+      submitted: false,
+    }
+  },
+  methods: {
+    readWord(event: any, russian: string) {
+      this.answer = event.target.value;
+      this.correct = this.answer === russian
+    },
+    submitWord(event: any) {
+      this.submitted = true;
     }
   }
 }
