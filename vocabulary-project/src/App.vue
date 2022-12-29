814234657b7424407184dfc67e7b3e46acc1631a
victor-at-wiris <victor.santiago@wiris.com>
2022-12-29 11:06:38 +0100

    feat: read from a json file and be able to write by key-value

--- vocabulary-project/src/App.vue
+++ vocabulary-project/src/App.vue
@@ -1,53 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h1>Hello World</h1>
+  <div>
+    <div v-for="(value, key) in myJson">
+      <div v-for="(english, russian) in value">KEY: {{ key }} VALUE: english {{ english }} russian {{ russian }}</div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script lang="ts">
+import json from './assets/vocab.json'
+export default {
+  data() {
+    return {
+      myJson: json
+    }
+  }
+}
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
